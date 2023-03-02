f6512c16c576211592d8d1339fbde44bea541521
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-03-01 18:45:10 -0600

    Actualizado mejor deteccion de usuario.

--- modules/search.py
+++ modules/search.py
@@ -2,6 +2,8 @@
 
 import requests
 import time
+import re
+import os
 
 class Colores:
   red="\033[31;1m"
@@ -10,135 +12,14 @@ class Colores:
   magenta="\033[36m"
   amarillo="\033[33m"
   
-print(f'{Colores.verde}[~] Atencion no siempre los resultados son 100% precisos')
-usuario = input(f'{Colores.red}[~] Ingresa el nombre del usuario: ')
+#print(f'{Colores.verde}[~] Atencion no siempre los resultados son 100% precisos')
+usuario = input(f'\n{Colores.red}[~] Ingresa el nombre del usuario: ')
 print(' ')
-url = "https://www.instagram.com/" + usuario
-response = requests.get(url)
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Instagram!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Instagram!')
-    time.sleep(2)
-  #-----------------Twitter-------------------
-print(' ')
-url = "https://twitter.com/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Twitter!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Twitter!')
-    time.sleep(2)
-  #--------------Reditt------------------
-print(' ')
-url = "https://www.reddit.com/user/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en reddit!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en reddit!')
-    time.sleep(2)
-  #--------------Pintirest--------------
-print(' ')
-url = f"https://www.pinterest.com/{usuario}/"
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Pinterest!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Pinterest!')
-    time.sleep(2)
-  #-----------------Pornhub------------
-print(' ')
-url = "https://pornhub.com/users/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Pornhub!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Pornhub!')
-    time.sleep(2)
-  #--------------------Youtube-----------
-print(' ')
-url = "https://www.youtube.com/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Youtube!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Youtube!')
-    time.sleep(2)
-  #------------------Xbox------------------
-print(' ')
-url = "https://xboxgamertag.com/search/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Xbox!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Xbox!')
-    time.sleep(2)
-  #---------------------Spotify------------
-print(' ')
-url = "https://open.spotify.com/user/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Spotify!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Spotify!')
-    time.sleep(2)
-  #------------------Patreon-------------
-print(' ')
-url = "https://www.patreon.com/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Patreon!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Patreon!')
-    time.sleep(2)
-  #------------Myspace-----------------------
-print(' ')
-url = "https://myspace.com/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Myspace!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Myspace!')
-    time.sleep(2)
-  #---------------Myanimelist----------------
-print(' ')
-url = "https://myanimelist.net/profile/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Myanimelist!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Myanimelist!')
-    time.sleep(2)
-  #-----------------Github--------------
-print(' ')
-url = "https://github.com/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Github!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Github!')
-    time.sleep(2)
-  #----------------Twitch-----------
-print(' ')
-url = "https://www.twitch.tv/" + usuario
-if response.status_code == 200:
-    response.close()
-    print(f'[~] Usuario encontrado en Twitch!: {url}')
-    time.sleep(2)
-else:
-    print('[~] Usuario no encontrado en Twitch!')
-    time.sleep(2)
+sites = [f"https://github.com/{usuario}", f"https://twitter.com/{usuario}", f"https://instagram.com/{usuario}", f"https://www.reddit.com/user/{usuario}", f"https://www.pinterest.com/{usuario}", f"https://www.twitch.tv/{usuario}", f"https://xboxgamertag.com/search/{usuario}", f"https://open.spotify.com/user/{usuario}", f"https://www.roblox.com/user.aspx?username={usuario}", f"https://t.me/{usuario}", f"https://xvideos.com/profiles/{usuario}", f"https://www.youtube.com/user/{usuario}", f"https://gitlab.com/{usuario}", f"https://api.mojang.com/users/profiles/minecraft/{usuario}", f"https://www.codecademy.com/profiles/{usuario}", f"https://www.codewars.com/users/{usuario}", f"https://forum.hackthebox.eu/profile/{usuario}", f"https://replit.com/@{usuario}"]
+for url in sites:
+    pagina = requests.get(url)
+    final = re.findall(usuario, pagina.text)
+    if final:
+        print(f'\n{Colores.verde}[  ✔️   ] Usuario encontrado! {url}')
+    else:
+        print(f'\n{Colores.red}[!] Usuario no encontrado')
