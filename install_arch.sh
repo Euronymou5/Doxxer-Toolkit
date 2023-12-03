b758517a050d623b9c01bcf70dd54ef33059e911
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-12-02 21:35:45 -0600

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+function is_installed() {
+    if pacman -Qi $1 &>/dev/null; then
+        echo -e "\033[32m$1 ya está instalado.\033[0m"
+    else
+        echo -e "\nInstalando $1..."
+        pacman -S $1 --noconfirm
+    fi
+}
+
 function install() {
-   clear
+    clear
 
-   echo -e "\nInstalando paquetes básicos..."
-   pacman -S --noconfirm python3 php wget
+    is_installed "python"
+    is_installed "python3"
+    is_installed "php"
+    is_installed "wget"
 
-   echo -e "\nInstalando requerimientos..."
-   pip3 install -r requirements.txt
+    echo -e "\nInstalando requerimientos..."
+    pip3 install -r requirements.txt
 
-   echo -e "\nInstalando cloudflared..."
-   python3 install_cfd.py
+    echo -e "\nInstalando cloudflared..."
+    python3 install_cfd.py
 
-   echo -e "\n\033[32mInstalación completa."
-   echo -e "\n[~] Utiliza el comando: python3 dox.py para ejecutar la herramienta."
+    echo -e "\n\033[32mInstalación completa.\033[0m"
+    echo -e "\n[~] Utiliza el comando: python3 dox.py para ejecutar la herramienta."
 }
 
 install
