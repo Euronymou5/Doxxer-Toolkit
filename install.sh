9b037597d3743c484e1623df8b6cd1e2f488488d
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-12-02 21:48:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,35 +1,42 @@
+#!/bin/bash
+
+function is_installed() {
+    if dpkg -l $1 &>/dev/null; then
+        echo -e "\033[32m\n$1 ya está instalado.\033[0m"
+    else
+        echo -e "\033[32m\nInstalando $1..."
+        apt install $1 -y
+    fi
+}
+
 function install() {
-   clear
-   echo -e "\033[31mActualizando paquetes..."
-   apt update
+    clear
 
-   echo -e "\nInstalando python..."
-   apt install python -y
+    echo -e "\033[31mActualizando paquetes..."
+    apt update
 
-   echo -e "\nInstalando python3..."
-   apt install python3 -y
+    is_installed "python"
+    is_installed "python3"
+    is_installed "php"
 
-   echo -e "\nInstalando php..."
-   apt install php -y
+    which pip3 > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+        echo -e "\033[32m\npip3 ya está instalado."
+    else
+        echo -e "\033[32m\nInstalando pip3..."
+        wget https://bootstrap.pypa.io/get-pip.py
+        sudo python3 get-pip.py
+        rm -rf get-pip.py
+    fi
 
-   which pip3 > /dev/null 2>&1
-   if [ "$?" -eq "0" ]; then
-   echo -e "\npip3 ya esta instalado."
-   else
-   echo -e "\nInstalando pip3...."
-   wget https://bootstrap.pypa.io/get-pip.py
-   python3 get-pip.py
-   rm -rf get-pip.py
-   fi
+    echo -e "\033[32m\nInstalando requerimientos...\n"
+    pip3 install -r requirements.txt
 
-   echo -e "\nInstalando requerimientos..."
-   pip3 install -r requirements.txt
+    echo -e "\033[32m\nInstalando cloudflared..."
+    python3 install_cfd.py
 
-   echo -e "\nInstalando cloudflared..."
-   python3 install_cfd.py
-
-   echo -e "\n\033[32mInstalacion completa."
-   echo -e "\n[~] Utiliza el comando: python3 dox.py para ejecutar la herramienta."
+    echo -e "\n\033[32mInstalación completa.\033[0m"
+    echo -e "\033[34m\n[~] Utiliza el comando: python3 dox.py para ejecutar la herramienta."
 }
 
 install
