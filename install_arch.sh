1b46da41e4b3db0feb72a09b27ac4bdf05da9cac
Euronymou5 <85043356+Euronymou5@users.noreply.github.com>
2023-12-02 21:31:07 -0600

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -1,17 +1,10 @@
+#!/bin/bash
+
 function install() {
    clear
 
-   echo -e "\nInstalando python..."
-   pacman -S python
-
-   echo -e "\nInstalando python3..."
-   pacman -S python3
-
-   echo -e "\nInstalando php..."
-   pacman -S php
-   
-   echo -e "\nInstalando wget..."
-   pacman -S wget
+   echo -e "\nInstalando paquetes básicos..."
+   sudo pacman -S --noconfirm python3 php wget
 
    echo -e "\nInstalando requerimientos..."
    pip3 install -r requirements.txt
@@ -19,7 +12,7 @@ function install() {
    echo -e "\nInstalando cloudflared..."
    python3 install_cfd.py
 
-   echo -e "\n\033[32mInstalacion completa."
+   echo -e "\n\033[32mInstalación completa."
    echo -e "\n[~] Utiliza el comando: python3 dox.py para ejecutar la herramienta."
 }
 
